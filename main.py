1f4bdfd582fc2e67431689aaca0516906f0f7009
AkiyaKiko <sinchialveno@protonmail.com>
2023-08-23 00:17:47 +0800

    fix

--- main.py
+++ main.py
@@ -125,8 +125,8 @@ def verify(new_user : types.User):
 #chat_member_handler. When status changes, telegram gives update. check status from old_chat_member and new_chat_member.
 @bot.chat_member_handler()
 def chat_m(message: types.ChatMemberUpdated):
-    # global Running
-    # global timeout
+    global Running
+    global timeout
     old = message.old_chat_member
     new = message.new_chat_member
     if new.status == "member":
